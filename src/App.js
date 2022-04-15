ff9d924252b0d9cff7e3aaab0b769fee580cd54a
Elxan Quliyev <qelxan177@gmail.com>
2022-04-15 11:08:44 -0700

    04.15.2022

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.scss';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import HeroSlider from './components/hero-slider/HeroSlider';
-import Home from './pages/Home';
+import { BrowserRouter } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import MyRoute from "./MyRoutes/MyRoute";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <div>
-        <Header/>
-        <Home/>
-        <Footer/>
-    </div>
-    );  
+    <BrowserRouter>
+      <Header />
+      <MyRoute />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import HeroSlider from '../components/hero-slider/HeroSlider'
+import React from "react";
+import HeroSlider from "../components/hero-slider/HeroSlider";
+import ProductList from "../components/product-list/ProductList";
 
 const Home = () => {
   return (
     <div>
-        <HeroSlider/>
+      <HeroSlider />
 
-        
+      <hr />
+      <ProductList categoryName="category" bannerName="categoryBanners"/>
+      <ProductList categoryName="seller" bannerName="sellerBanners"/>
+
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
