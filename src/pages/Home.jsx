cee5f63079ccdce6a3257b404cb03561cd7e1437
Elxan Quliyev <qelxan177@gmail.com>
2022-04-18 10:21:32 -0700

    04.18.2022

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HeroSlider from "../components/hero-slider/HeroSlider";
 import ProductList from "../components/product-list/ProductList";
 import BasicModal from "../components/basic-modal/BasicModal";
 const Home = () => {
-
   // useEffect(()=>{
   //   console.log("component mount edildi")
   // },[])
@@ -15,25 +14,30 @@ const Home = () => {
   //   }
   // })
 
-  
-  const [active, setActive] = useState(false)
-  const onClose=()=>{
-    setActive(false)
-  }
+  // const [active, setActive] = useState(false)
+  // const onClose=()=> setActive(false)
+  const myButton = useRef();
+  // const myBox=useRef()
+  useEffect(() => {
+    myButton.current.onclick = (e) =>
+      e.target.parentNode.querySelector(".box").classList.toggle("my-active");
+  }, []);
   return (
-    <div>
-        {/* <div className={`box ${ active ? "my-active" : ""}`}></div>
-        <button onClick={()=>setActive(act=>!act)}>Click</button> */}
-        <button onClick={()=>setActive(true)}>Modal Toggle</button>
-        <BasicModal onClose={onClose} act={active}/>
+    <div className="tt">
+      <div className="box"></div>
+      <button ref={myButton}>Click</button>
+      {/* <button ref={myButton}>Modal Toggle</button> */}
+      {/* <BasicModal onClose={onClose} act={active}/> */}
     </div>
   );
 };
 
 export default Home;
 
-    {/* <HeroSlider />
+{
+  /* <HeroSlider />
 
       <hr />
       <ProductList categoryName="category" bannerName="categoryBanners"/>
-      <ProductList categoryName="seller" bannerName="sellerBanners"/> */}
+      <ProductList categoryName="seller" bannerName="sellerBanners"/> */
+}
